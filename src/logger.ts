f7e46dc831ca0ae42b115b129ac1f8eec9ff988e
Biraj <biraj.pub@gmail.com>
2025-10-01 03:18:59 +0530

    fix(logger): add timestamp

--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,24 @@
 import { EXT_NAME } from "./constants";
 
+const tag = () => `[${EXT_NAME}] (${new Date().toLocaleString()})`;
+
 /**
  * Logger wrapper that prefixes all console methods with the extension name
  */
 export const logger = {
   log: (...args: any[]) => {
-    console.log(`[${EXT_NAME}]`, ...args);
+    console.log(tag(), ...args);
   },
 
   warn: (...args: any[]) => {
-    console.warn(`[${EXT_NAME}]`, ...args);
+    console.warn(tag(), ...args);
   },
 
   debug: (...args: any[]) => {
-    console.debug(`[${EXT_NAME}]`, ...args);
+    console.debug(tag(), ...args);
   },
 
   error: (...args: any[]) => {
-    console.error(`[${EXT_NAME}]`, ...args);
+    console.error(tag(), ...args);
   },
 };
